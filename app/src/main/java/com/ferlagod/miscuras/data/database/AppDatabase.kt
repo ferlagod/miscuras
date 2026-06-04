5f35b98cf6c0d80336409678fd3d1c24c2f18d26
ferlagod <info@bibliohouse.org>
2026-06-04 16:25:04 +0200

    feat: Se añaden las siguientes categorías:
    - Se añade una ncategoría "Limpieza de heridas" donde se incluye el Prontosan, con lo cual cambia la lógica del algoritmo para corregir errores.
    - Se añade una  categoría Superabsroventes, donde se incluye dos nuevos productos.
    - Se añade  categoría Carbón y plata, donde se integra un producto.
    - Se añade categoría Colágeno donde se añaden nuevos productos.

--- app/src/main/java/com/ferlagod/miscuras/data/database/AppDatabase.kt
+++ app/src/main/java/com/ferlagod/miscuras/data/database/AppDatabase.kt
@@ -23,7 +23,7 @@ import java.io.InputStreamReader
  */
 @Database(
     entities = [ReglaEntity::class, ApositoEntity::class],
-    version = 18,
+    version = 19,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
