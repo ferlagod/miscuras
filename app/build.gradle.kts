998119a0373823e3d1ca9e3e7bf07cb99435c20d
ferlagod <ferlagod@example.com>
2026-09-25 16:25:50 +0200

    chore: nueva versión 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ferlagod.miscuras"
         minSdk = 26
         targetSdk = 36
-        versionCode = 202609131
-        versionName = "1.0.7"
+        versionCode = 202609251
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
